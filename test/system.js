40bb0cfded7852d03d70f66ccbf7d6155147ba9b
Callum Locke <callum.locke@gmail.com>
2014-08-20 18:19:58 +0100

    new api, refactor

--- test/system.js
+++ test/system.js
@@ -9,6 +9,7 @@ var expect = require('chai').expect;
 var exec = require('child_process').exec;
 
 describe('trip.js', function () {
+
   var cliPath = path.resolve(__dirname, '..', 'bin', 'cli.js');
 
   before(function (done) {
@@ -30,45 +31,57 @@ describe('trip.js', function () {
     done();
   });
 
-  it('can run actions in parallel', function (done) {
+
+  it('can run actions in series and parallel', function (done) {
     exec(cliPath + ' parallel-actions', function (err, stdout, stderr) {
       console.log('\n\n=== STDOUT:\n', stdout, '\n=== /STDOUT');
       console.log('\n\n=== STDERR:', stderr, '\n=== /STDERR');
 
-      var lines = stdout.split('\n');
-      expect(lines[2]).to.equal('thing 1');
-      expect(lines[3]).to.equal('thing 2');
-      expect(lines[4]).to.equal('thing 3');
-      expect(lines[5]).to.equal('thing 4');
+      var lines = stdout.split('\n').filter(function (line) {
+        return line.length && line.charAt(0) !== '[';
+      });
+
+      expect(lines[0]).to.equal('thing 1');
+      expect(lines[1]).to.equal('thing 2');
+      expect(lines[2]).to.equal('thing 3');
+      expect(lines[3]).to.equal('thing 4');
       expect(stderr).to.equal('');
       done();
     });
   });
 
-  it('actions can receive input from cli and from previous actions', function (done) {
-    exec(cliPath + ' task-input:"from command line"', function (err, stdout, stderr) {
+
+  it('actions can receive targets from cli and from previous actions', function (done) {
+    exec(cliPath + ' task-input:"from command line":"hi"', function (err, stdout, stderr) {
       console.log('\n\n=== STDOUT:\n', stdout, '\n=== /STDOUT');
       console.log('\n\n=== STDERR:', stderr, '\n=== /STDERR');
 
-      var lines = stdout.split('\n');
-      expect(lines).to.include('action 1 from command line'); // can receive params from cli
-      expect(lines).to.include('action 2 info from action 1'); // can receive params from previous task
+      var lines = stdout.split('\n').filter(function (line) {
+        return line.length && line.charAt(0) !== '[';
+      });
+
+      expect(lines[0]).to.equal('action 1 from command line hi'); // can receive params from cli
+      expect(lines[1]).to.equal('action 2 message from action 1'); // can receive params from previous task
       expect(stderr).to.equal('');
       done();
     });
   });
 
 
-  it('actions can be synchronous, and handle input', function (done) {
-    exec(cliPath + ' sync-actions:"from command line"', function (err, stdout, stderr) {
+  it ('can run parallel actions straight from the command line', function (done) {
+    exec(cliPath + ' say-thing-1,say-thing-2', function (err, stdout, stderr) {
       console.log('\n\n=== STDOUT:\n', stdout, '\n=== /STDOUT');
       console.log('\n\n=== STDERR:', stderr, '\n=== /STDERR');
 
-      var lines = stdout.split('\n');
-      expect(lines).to.include('action 1 from command line'); // can receive params from cli
-      expect(lines).to.include('action 2 info from action 1'); // can receive params from previous task
+      var lines = stdout.split('\n').filter(function (line) {
+        return line.length && line.charAt(0) !== '[';
+      });
+
+      expect(lines[0]).to.equal('thing 1'); // can receive params from cli
+      expect(lines[1]).to.equal('thing 2'); // can receive params from previous task
       expect(stderr).to.equal('');
       done();
     });
   });
+
 });
